8aac2b47b590778649564609f38a1a406fdaf421
Michael Nwani <kmichael24@gmail.com>
2018-04-19 01:51:26 -0400

    update Cohort models backfill method

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,19 +1,16 @@
 class Cohort < ApplicationRecord
   ONE_WEEK = 7
 
-  default_scope { order(start: :desc) }
-  default_scope { where("users > 0") }
-
   has_many :buckets
 
   def self.backfill
-    ActiveRecord::Base.connection.execute("TRUNCATE cohorts RESTART IDENTITY")
-    today = Date.today
+    ActiveRecord::Base.connection.execute("TRUNCATE cohorts RESTART IDENTITY CASCADE")
+    last_date = User.maximum(:created_at).to_date
 
     start_date = User.minimum(:created_at).to_date # 2012-05-16
     end_date = start_date+ONE_WEEK # 2012-05-24
 
-    while (end_date < today)
+    while (start_date < last_date)
       users = User.where(created_at: start_date..end_date) # number of signups
       Cohort.create!(start: start_date,
                      end: end_date,
